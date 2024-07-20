1857d285d22e29c0ec6ed18eb1c11c3c2923a3a8
amakodev <amakodev@gmail.com>
2024-07-20 12:18:51 +0200

    Responsive Design and UI enhancements

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const AppHeader: React.FC = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header className="AppHeader">
       <Link to="">
-        <img src={logo} alt="HandDogSocial Logo" className="logo" />
+        <img src={logo} alt="HandDogSocial Logo" className="AppHeader__logo" />
       </Link>
-      <nav className="nav">
+      <button className="AppHeader__mobile-menu-button" onClick={toggleMobileMenu}>
+        ☰
+      </button>
+      <nav className={`AppHeader__nav ${isMobileMenuOpen ? "open" : ""}`}>
         <Link to="about">About</Link>
         <Link to="features">Features</Link>
         <Link to="how">How It Works</Link>
         <Link to="community">Community</Link>
         <Link to="signup">Sign Up</Link>
+        <button className="AppHeader__cta-button">Enter HDS</button>
       </nav>
-      <button className="cta-button">Sign Up/Login</button>
     </header>
   );
 };
